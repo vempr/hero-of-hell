7c5bef4b135589ee6501409939b54dc6aec67a02
vempr <alexnguyena6@gmail.com>
2026-04-03 20:11:39 +0200

    more animations for switching between dog and devil form, using chainsaw asset - fixed gradual sprint acceleration, before it was instant

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 ## Credits
 - Terrain from https://sketchfab.com/3d-models/big-terrain-50338f06da754fd29481b0bdf86b7f29 and made by https://sketchfab.com/Dogus.Berk.Bayram
 - Doors: https://sketchfab.com/3d-models/low-poly-psx-style-worn-wooden-doors-pack-16dd7631d2e145ffacfc374961d71cf1
+- Chainsaw: https://sketchfab.com/3d-models/animated-chainsaw-2c0d25112fbb4a2c9dc5610dd8a30dab
--- scripts/player.gd
+++ scripts/player.gd
@@ -16,6 +16,9 @@ const DEVIL_MAX_STAMINA: float = 5.0
 const STAMINA_DRAIN: float = 1.0
 const STAMINA_REGEN: float = 0.5
 
+const ACCEL: float = 6.0
+const DECEL: float = 8.0
+
 var SPEED: float
 var JUMP_VELOCITY: float
 var target_speed: float
@@ -25,6 +28,7 @@ var current_form: FORM
 var stamina: float
 var max_stamina: float
 var is_sprinting: bool = false
+var sprint_factor: float = 1.0
 
 @onready var camera: Camera3D = %Camera
 @onready var cooldown_timer: Timer = %TransformCooldownTimer
@@ -36,7 +40,7 @@ var camera_sens: float = 0.1
 
 func _ready() -> void:
 	current_form = FORM.DOG
-	SPEED = DOG_JUMP
+	SPEED = DOG_SPEED
 	JUMP_VELOCITY = DOG_JUMP
 	target_speed = SPEED
 	target_jump = JUMP_VELOCITY
@@ -44,6 +48,10 @@ func _ready() -> void:
 	stamina = max_stamina
 	
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	
+	%FrontChainsaw.visible = false
+	%LeftChainsaw.visible = false
+	%RightChainsaw.visible = false
 
 
 func _physics_process(delta: float) -> void:
@@ -56,18 +64,22 @@ func _physics_process(delta: float) -> void:
 	if Input.mouse_mode == Input.MOUSE_MODE_VISIBLE:
 		return
 	
-	SPEED = move_toward(SPEED, target_speed, abs(target_speed - SPEED) * delta)
-	JUMP_VELOCITY = move_toward(JUMP_VELOCITY, target_jump, abs(target_jump - JUMP_VELOCITY) * delta)
+	var step: float = abs(DOG_SPEED - DEVIL_SPEED) * 5.0 * delta
+	SPEED = move_toward(SPEED, target_speed, step)
+	JUMP_VELOCITY = move_toward(JUMP_VELOCITY, target_jump, step)
 	
 	var sprint_input := Input.is_action_pressed("sprint")
 	is_sprinting = sprint_input and stamina > 0.1 and _is_moving()
 	if is_sprinting:
-		
 		stamina = max(stamina - STAMINA_DRAIN * delta, 0.0)
 	else:
 		stamina = min(stamina + STAMINA_REGEN * delta, max_stamina)
 	
-	var current_speed := SPEED * (SPRINT_MULTIPLIER if is_sprinting else 1.0)
+	var target_sprint := SPRINT_MULTIPLIER if is_sprinting else 1.0
+	var rate := ACCEL if is_sprinting else DECEL
+	sprint_factor = move_toward(sprint_factor, target_sprint, rate * delta)
+	
+	var current_speed := SPEED * sprint_factor
 	
 	if not is_on_floor():
 		velocity += get_gravity() * 6.0 * delta
@@ -105,6 +117,9 @@ func _input(event: InputEvent):
 				max_stamina = DEVIL_MAX_STAMINA
 				stamina = min(stamina, max_stamina)
 				
+				%FrontChainsaw.visible = true
+				%LeftChainsaw.visible = true
+				%RightChainsaw.visible = true
 				animation.play("dog_out")
 			FORM.DEVIL:
 				current_form = FORM.DOG
